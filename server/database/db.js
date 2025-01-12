e337ca6ae2e16637a2f5561fef023a34c21b55fb
ryanmartinez <martinez274@usf.edu>
2025-01-12 16:26:14 -0500

    Added support for Neon, online database hosting

--- server/database/db.js
+++ server/database/db.js
@@ -1,11 +1,30 @@
-const Pool = require("pg").Pool;
+require('dotenv').config();
+
+const { Pool } = require('pg');
+
+const { PGHOST, PGDATABASE, PGUSER, PGPASSWORD } = process.env;
 
 const pool = new Pool({
-    user: "postgres",
-    password: "postgres",
-    host: "localhost",
-    port: 5432,
-    database: "pernwebapp"
+  host: PGHOST,
+  database: PGDATABASE,
+  username: PGUSER,
+  password: PGPASSWORD,
+  port: 5432,
+  ssl: {
+    require: true,
+  },
 });
 
-module.exports = pool;
+async function getPgVersion() {
+  const client = await pool.connect();
+  try {
+    const result = await client.query('SELECT version()');
+    console.log(result.rows[0]);
+  } finally {
+    client.release();
+  }
+}
+
+getPgVersion();
+
+module.exports = pool
