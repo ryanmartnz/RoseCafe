948c33c1839f43493ba4b1f34b2b8a1d35c15968
ryanmartinez <martinez274@usf.edu>
2025-01-12 18:18:27 -0500

    Updated database user

--- server/database/db.js
+++ server/database/db.js
@@ -19,7 +19,7 @@ async function getPgVersion() {
   const client = await pool.connect();
   try {
     const result = await client.query('SELECT version()');
-    console.log(result.rows[0]);
+    // console.log(result.rows[0]);
   } finally {
     client.release();
   }
