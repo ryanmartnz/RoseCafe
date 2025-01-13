d4725b6937b9dc17249a2fcbbc0ec42fde9af38e
ryanmartinez <martinez274@usf.edu>
2025-01-13 17:48:59 -0500

    Custom domain

--- server/index.js
+++ server/index.js
@@ -6,7 +6,7 @@ const cookieParser = require("cookie-parser");
 // Middleware
 
 app.use(express.json()); // use req.body
-app.use(cors({ credentials: true, origin: "https://rosecafesite.onrender.com"} ));
+app.use(cors({ credentials: true, origin: "rosecafe.tech"} ));
 app.use(cookieParser());
 
 // Routes
