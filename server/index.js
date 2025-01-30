0e7164af18b6597f02e5e537705e59f263a5e88a
ryanmartinez <martinez274@usf.edu>
2025-01-30 13:12:00 -0500

    added hack to keep instance alive

--- server/index.js
+++ server/index.js
@@ -30,6 +30,23 @@ app.use("/cart", require("./routes/cart"));
 //orders route
 app.use("/orders", require("./routes/orders"));
 
+// const url = "https://api.rosecafe.tech";
+const interval = 30000;
+
+const reloadWebsite = async () => {
+    const response = await fetch("https://api.rosecafe.tech/menu/drinks/hot-coffees", {
+        method: "GET",
+        credentials: "include"
+    });
+    if(response.ok) {
+        console.log(`Reloaded at ${new Date().toISOString()}: Status Code ${response.status}`);
+    } else {
+        console.error(`Error reloading at ${new Date().toISOString()}:`, response.statusText)
+    }
+}
+
+setInterval(reloadWebsite, interval)
+
 app.listen(3001, () => {
     console.log("Server is running on port 3001");
 });
